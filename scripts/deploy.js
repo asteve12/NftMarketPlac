b222035d67924f4463f3e56b20fb25019166492c
asteve <asteve@localhost.localdomain>
2022-08-02 17:52:02 +0100

    add frontend

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,18 +7,18 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const NFTMarket = await hre.ethers.getContractFactory("NFTMarket");
+  const nftMarket = await NFTMarket.deploy()
+  await nftMarket.deployed();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  console.log("nftMarket deployed to:", nftMarket.address)
+  
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const NFT = await hre.ethers.getContractFactory("NFT");
+  const nft = await NFT.deploy(nftMarket.address)
+  await nft.deployed();
 
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  console.log("nft deployed to:",nft.address)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- test/Lock.js
+++ test/Lock.js
@@ -6,39 +6,45 @@ const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("NFTMarket", function () {
-  it("Should create and execute sale", async function () {
+describe("NFTMarket", function() {
+  it("Should create and execute market sales", async function() {
     const Market = await ethers.getContractFactory("NFTMarket")
-    const market = await Market.deploy();
-    await market.deployed();
-
-    const marketAddress = market.address;
+    const market = await Market.deploy()
+    await market.deployed()
+    const marketAddress = market.address
 
     const NFT = await ethers.getContractFactory("NFT")
     const nft = await NFT.deploy(marketAddress)
-    await nft.deployed();
+    await nft.deployed()
+    const nftContractAddress = nft.address
 
-    const nftContractAddr = nft.address;
-
-    let listingPrice = await market.getListingPrice();
+    let listingPrice = await market.getListingPrice()
     listingPrice = listingPrice.toString()
 
-    const auctionPrice = ethers.utils.parseUnits("1", "ether")
+    const auctionPrice = ethers.utils.parseUnits('1', 'ether')
 
     await nft.createToken("https://www.mytokenlocation.com")
     await nft.createToken("https://www.mytokenlocation2.com")
-    
-    await market.createMarketItem(nftContractAddr, 1, auctionPrice, { value: listingPrice })
-    await market.createMarketItem(nftContractAddr, 2, auctionPrice, { value: listingPrice })
-    
-    const [_, buyerAddress] = await ethers.getSigners();
-
-    await market.connect(buyerAddress).createMarketSale(nftContractAddr, 1, {value:auctionPrice})
-
-    const items = await market.fetchMarketItems()
-    
-    console.log("items",items)
-
-  })
   
-});
+    await market.createMarketItem(nftContractAddress, 1, auctionPrice, { value: listingPrice })
+    await market.createMarketItem(nftContractAddress, 2, auctionPrice, { value: listingPrice })
+    
+    const [_, buyerAddress] = await ethers.getSigners()
+
+    await market.connect(buyerAddress).createMarketSale(nftContractAddress, 1, { value: auctionPrice})
+
+    items = await market.fetchMarketItems()
+    items = await Promise.all(items.map(async i => {
+      const tokenUri = await nft.tokenURI(i.tokenId)
+      let item = {
+        price: i.price.toString(),
+        tokenId: i.tokenId.toString(),
+        seller: i.seller,
+        owner: i.owner,
+        tokenUri
+      }
+      return item
+    }))
+    console.log('items: ', items)
+  })
+})
